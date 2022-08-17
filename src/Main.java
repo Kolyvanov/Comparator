03c0ba18da579b18809470999fbeed2f6e75677e
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-08-17 14:07:18 +0300

    Add maxSurname

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,11 @@ public class Main {
         persons.add(new Person("Джон", "Дональд Трамп", 80));
         persons.add(new Person("Александр", "Алексеевич Алексеев", 53));
         persons.add(new Person("Саша", "Петров", 10));
+        persons.add(new Person("Муса", "Ходжи Расулович Газматов", 97));
 
         System.out.println(persons);
 
-        persons.sort(new PersonComparator());
+        persons.sort(new PersonComparator(3));
         System.out.println(persons);
     }
 }
